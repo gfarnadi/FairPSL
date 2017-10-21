17d06acc9a4c80978cc5cb568fd7e391777f3359
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2017-10-22 02:03:53 +0330

    removing extra rules

--- debug_inference/tinypsl/link.py
+++ debug_inference/tinypsl/link.py
@@ -4,6 +4,7 @@
 import sys
 from tiny_utils import read_link_data, write_link_mpe
 from tiny_mip import solve_mip
+#from tiny_cvx import solve_cvx
 
 def main(outfilename):
 
@@ -28,57 +29,52 @@ def main(outfilename):
     # 20:  Lived(P1,L) & Lived(P2,L) & P1!=P2   -> Knows(P1,P2)
     ground_rules = [(lived_rel[(A, C)],
                      lived_rel[(B, C)],
-                     (True, float(A!=B)),
                      knows_rel[(A, B)])
                     for A in people
                     for B in people
                     for C in places
                     if A!=B]
-    signs = [False, False, False, False]
+    signs = [False, False, False]
     weight = 20.0
     r_list += get_list(weight, ground_rules, signs)
     
     # 5:  Lived(P1,L1) & Lived(P2,L2) & P1!=P2 & L1!=L2  -> !Knows(P1,P2)
     ground_rules = [(lived_rel[(A, C)],
                      lived_rel[(B, D)],
-                     (True, float(A!=B)),
-                     (True, float(C!=D)),
                      knows_rel[(A, B)])
                     for A in people
                     for B in people
                     for C in places
                     for D in places
-                    if A!=B
+                    if (A!=B and C!=D)
                     ]
-    signs = [False, False, False, False, True]
+    signs = [False, False, True]
     weight = 5.0
     r_list += get_list(weight, ground_rules, signs)
     
     # 10:  Likes(P1,L) & Likes(P2,L) & P1!=P2  -> Knows(P1,P2)
     ground_rules = [(likes_rel[(A, C)],
                      likes_rel[(B, C)],
-                     (True, float(A!=B)),
                      knows_rel[(A, B)])
                     for A in people
                     for B in people
                     for C in interests
                     if A!=B
                     ]
-    signs = [False, False, False, False]
+    signs = [False, False, False]
     weight = 10.0
     r_list += get_list(weight, ground_rules, signs)
     
     # 5:   Knows(P1,P2) & Knows(P2,P3) & P1!=P3 -> Knows(P1,P3)
     ground_rules = [(knows_rel[(A, B)],
                      knows_rel[(B, C)],
-                     (True, float(A!=C)),
                      knows_rel[(A, C)])
                     for A in people
                     for B in people
                     for C in people
                     if (A!=B and B!=C and A!=C)]
     
-    signs = [False, False, False, False]
+    signs = [False, False, False]
     weight = 5.0
     r_list += get_list(weight, ground_rules, signs)
     
