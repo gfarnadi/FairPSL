76ced0d4478adf7ed88cd2f683cace2c658ba5b7
Golnoosh <Gfarnadi@Golnooshs-MacBook-Pro.local>
2017-10-28 03:40:51 -0700

    add accuracy

--- fair_evaluation.py
+++ fair_evaluation.py
@@ -26,10 +26,38 @@ def calculate(counts,result):
     p2 = (c/n2)
     return p1,p2
 
-def evaluate(result, counts):
+def evaluate(result, counts, fairMeasureCode):
     p1,p2 = calculate(counts,result)
     RR = p1-p2
     RD = p1/p2
     RC = (1-p1)/(1-p2)
-    return RR,RD,RC
-    
+    if fairMeasureCode=='RR':
+        return RR
+    elif fairMeasureCode == 'RD':
+        return RD
+    elif fairMeasureCode =='RC':
+        return RC
+  
+  
+def accuracy(dataPath, result, atoms): 
+    papers = []
+    with open(dataPath+'paper.txt') as f:
+        for line in f:
+            line = line.strip()
+            if not line: continue
+            papers.append(line.split()[0])
+    vardic = atoms['acceptable']
+    score = 0.0
+    for paper in papers:
+        var = vardic[paper][1]
+        if var in result:
+            predict = result[var]
+            truth = vardic[var]
+            if round(predict, 1)>0.5:
+                if truth ==1.0:
+                    score+=1.0
+            else:
+                if truth ==0.0:
+                    score+=1.0
+    score = float(score) / float(len(papers))
+    return score
