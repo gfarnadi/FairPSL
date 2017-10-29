94cd65e558a541a55dcf96ada2de1ad152feb5df
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2017-10-29 05:03:16 +0330

    double-checking delta-fair constraints

--- fair_measure.py
+++ fair_measure.py
@@ -9,22 +9,19 @@ def calculate(counts,vid_dict):
     a = 0.0
     c = 0.0
     for f1,f2,d in counts:
-        f1f2 = max(f1+f2-1,0)
-        f1nf2 = max(f1-f2,0)
+        f1f2 = max(f1+f2-1, 0)
+        f1nf2 = max(f1-f2, 0)
         n1 += f1f2
         n2 += f1nf2
         if d[0]:
-            a+= max(f1f2 - d[1],0)
-            c+= max(f1nf2 - d[1],0)
+            a += max(f1f2 - d[1], 0)
+            c += max(f1nf2 - d[1], 0)
         else:
-            if f1f2==1:
-                a+= 1 - vid_dict[d[1]] 
-            else:
-                a+= 0
-            if f1nf2==1:
-                c+= 1 - vid_dict[d[1]] 
-            else:
-                c+=0
+            if f1f2 == 1:
+                a += 1 - vid_dict[d[1]] 
+            if f1nf2 == 1:
+                c += 1 - vid_dict[d[1]]
+
     return a,c,n1,n2
 
 def riskDifferenceObjective(counts,vid_dict,epsilon):
@@ -44,8 +41,8 @@ def riskChanceObjective(counts,vid_dict,epsilon):
 def riskDifferenceConstraints(counts,vid_dict,epsilon):
     a,c,n1,n2 = calculate(counts,vid_dict)
     constraints = []
-    constraints.append((n2*a-n1*c - n1*n2*epsilon) <=0)
-    constraints.append((n2*a-n1*c + n1*n2*epsilon) >=0)
+    constraints.append((n2*a - n1*c - n1*n2*epsilon) <= 0)
+    constraints.append((n2*a - n1*c + n1*n2*epsilon) >= 0)
     return constraints
 
 
@@ -53,8 +50,8 @@ def riskDifferenceConstraints(counts,vid_dict,epsilon):
 def riskRatioConstraints(counts,vid_dict,epsilon):
     a,c,n1,n2 = calculate(counts,vid_dict)
     constraints = []
-    constraints.append(n2*a - (1+epsilon)*(n1*c) <=0)
-    constraints.append(n2*a - (1-epsilon)*(n1*c) >=0)
+    constraints.append((n2*a - (1+epsilon)*n1*c) <= 0)
+    constraints.append((n2*a - (1-epsilon)*n1*c) >= 0)
     return constraints
     
     
@@ -62,8 +59,8 @@ def riskRatioConstraints(counts,vid_dict,epsilon):
 def riskChanceConstraints(counts,vid_dict,epsilon):
     a,c,n1,n2 = calculate(counts,vid_dict)
     constraints = []
-    constraints.append(-n2*a+ (1+epsilon)*n1*c - epsilon*n1*n2 <=0)
-    constraints.append(-n2*a+ (1-epsilon)*n1*c + epsilon*n1*n2 >=0)
+    constraints.append((-n2*a + (1+epsilon)*n1*c - epsilon*n1*n2) <= 0)
+    constraints.append((-n2*a + (1-epsilon)*n1*c + epsilon*n1*n2) >= 0)
     return constraints
     
     
