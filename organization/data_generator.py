2cec44e219f27213fc0bf39d5f12b734889e8774
Golnoosh Farnadi <golnoosh@Golnooshs-MBP.lan>
2019-07-27 14:24:59 -0400

    fix data generator

--- organization/data_generator.py
+++ organization/data_generator.py
@@ -8,6 +8,7 @@ import os, shutil
 import numpy 
 import random
 from random import shuffle
+from __future__ import print_function
 
 
 # In[2]:
@@ -15,14 +16,14 @@ from random import shuffle
 
 def generate_qualification(user_dict, quality_file, quality_rate):
     quality_dict = {}
-    with open(quality_file,'w') as qf:
+    with open(quality_file,"w") as qf:
         for user in user_dict.keys():
-            if random.random()<(1-quality_rate):
-                quality_dict[user] = 0
-                print('%s\t0'%(user), file=qf)
-            else:
+            if random.random()<quality_rate:
                 quality_dict[user] = 1
-                print('%s\t1'%(user), file=qf)
+                print('%s\t1'%(user),file=qf)
+            else:
+                quality_dict[user] = 0
+                print('%s\t0'%(user),file=qf)
     return quality_dict
 
 
@@ -35,88 +36,169 @@ def read_user_data(user_file):
         lines = uf.readlines()
         for line in lines:
             info = line.split('\t')
-            user_dict[info[0]] = info[1]
+            user_dict[info[0]] = info[1].strip()
     return user_dict
 
 
 # In[4]:
 
 
-def generate_data(user_file, quality_file, feedback_file, submission_rate_f, submission_rate_m, feedback_equal_p, feedback_equal_n, feedback_notequal_p, feedback_notequal_n, quality_rate):
-    user_dict = read_user_data(user_file)
-    quality_dict = generate_qualification(user_dict, quality_file, quality_rate)
-    with open(feedback_file, "w") as ff:
-        #user1: is feedback giver and user2: is feedback reciever
-        for user1,label1 in user_dict.items():
-            for user2, label2 in user_dict.items():
-                if user1==user2:
-                    pass
-                else:
-                    if label1=='f':
-                        if random.random()<submission_rate_f:
-                            if label1==label2:
-                                if quality_dict[user2]==1:
-                                    if random.random()<feedback_equal_p:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                else:
-                                    if random.random()<feedback_equal_n:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                            else:
-                                if quality_dict[user2]==1:
-                                    if random.random()<feedback_notequal_p:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                else:
-                                    if random.random()<feedback_notequal_n:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                    else:
-                        if random.random()<submission_rate_m:
-                            if label1==label2:
-                                if quality_dict[user2]==1:
-                                    if random.random()<feedback_equal_p:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                else:
-                                    if random.random()<feedback_equal_n:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                            else:
-                                if quality_dict[user2]==1:
-                                    if random.random()<feedback_notequal_p:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                else:
-                                    if random.random()<feedback_notequal_n:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                
+def read_manager_data(manager_file):
+    manager_dict = {}
+    with open(manager_file, 'r') as mf:
+        lines= mf.readlines()
+        for line in lines:
+            info = line.split('\t')
+            user1 = info[0].strip()
+            user2 = info[1].strip()
+            if user2 in manager_dict.keys():
+                manager_dict[user2].append(user1)
+            else:
+                manager_dict[user2] = [user1]
+    return manager_dict
 
 
 # In[5]:
 
 
+def generate_feedback(user_dict, quality_dict, manager_dict,feedback_equal_mp, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, feedback_equal_p, feedback_equal_n, feedback_notequal_p, feedback_notequal_n, feedback_file):
+    feedback_dict = {}
+    with open(feedback_file, 'w') as ff:
+        for user1,label1 in user_dict.items():
+                for user2, label2 in user_dict.items():
+                    if user1==user2:
+                        pass
+                    else:
+                        if user2 in manager_dict.keys():
+                            if user1 in manager_dict[user2]:
+                                if quality_dict[user2]==1:
+                                    if label1==label2:
+                                        if random.random()<feedback_equal_mp:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
+                                    else:
+                                        if random.random()<feedback_notequal_mp:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
+                                else:
+                                    if label1==label2:
+                                        if random.random()<feedback_equal_mn:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
+                                    else:
+                                        if random.random()<feedback_notequal_mn:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
+                        else:     
+                            
+                            if quality_dict[user2]==1:
+                                if label1==label2:
+                                    if random.random()<feedback_equal_p:
+                                        print('%s\t%s\t1'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 1
+                                    else:
+                                        print('%s\t%s\t0'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 0
+                                else:
+                                    if random.random()<feedback_notequal_p:
+                                        print('%s\t%s\t1'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 1
+                                    else:
+                                        print('%s\t%s\t0'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 0
+                            else:
+                                if label1==label2:
+                                    if random.random()<feedback_equal_n:
+                                        print('%s\t%s\t1'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 1
+                                    else:
+                                        print('%s\t%s\t0'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 0
+                                else:
+                                    if random.random()<feedback_notequal_n:
+                                        print('%s\t%s\t1'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 1
+                                    else:
+                                        print('%s\t%s\t0'%(user1,user2), file =ff)
+                                        feedback_dict[(user1,user2)] = 0
+    return feedback_dict
+
+
+# In[6]:
+
+
+def generate_submission(user_file, manager_file, quality_file, feedback_file, submit_file, submission_rate_f, submission_rate_m, feedback_equal_p, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, quality_rate):
+    user_dict = read_user_data(user_file)
+    quality_dict = generate_qualification(user_dict, quality_file, quality_rate)
+    manager_dict = read_manager_data(manager_file)
+    feedback_dict = generate_feedback(user_dict, quality_dict, manager_dict,feedback_equal_mp, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, feedback_equal_p, feedback_equal_n, feedback_notequal_p, feedback_notequal_n, feedback_file)
+    with open(submit_file, "w") as sf:
+        for feedback, opinion in feedback_dict.items():
+            user1 = feedback[0]
+            user2 = feedback[1]
+            if user_dict[user1] =='f':
+                if random.random()<submission_rate_f:
+                    print('%s\t%s\t%d'%(user1,user2,opinion), file =sf)
+            else:
+                if random.random()<submission_rate_m:
+                    print('%s\t%s\t%d'%(user1,user2,opinion), file =sf)
+    return quality_dict
+
+
+# In[7]:
+
+
+def generate_performance(quality_dict, performance_rate_p, performance_rate_n, performance_file):
+    with open(performance_file, 'w') as pf:
+        for user,quality in quality_dict.items():
+            if quality_dict[user] == 1:
+                if random.random<performance_rate_p:
+                    print('%s\t%d'%(user,1), file =pf)
+                else:
+                    print('%s\t%d'%(user,0), file =pf)
+            else:
+                if random.random<performance_rate_n:
+                    print('%s\t%d'%(user,1), file =pf)
+                else:
+                    print('%s\t%d'%(user,0), file =pf)
+
+
+# In[8]:
+
+
 user_file = './data/gender.txt'
 quality_file = './data/1/quality.txt'
 feedback_file='./data/1/feedback.txt'
-feedback_equal_p = 0.9
+submit_file = './data/1/submit.txt'
+performance_file = './data/1/performance.txt'
+manager_file='./data/manages.txt'
+feedback_equal_mp = 0.9
+feedback_equal_mn = 0.05
+feedback_notequal_mp =0.6
+feedback_notequal_mn =0.01
+feedback_equal_p = 0.8
 feedback_equal_n = 0.05
 feedback_notequal_p =0.5
 feedback_notequal_n =0.01
 submission_rate_f = 0.1
-submission_rate_m = 0.3
+submission_rate_m = 0.2
 quality_rate = 0.3
-generate_data(user_file, quality_file, feedback_file, submission_rate_f, submission_rate_m, feedback_equal_p, feedback_equal_n, feedback_notequal_p, feedback_notequal_n, quality_rate)
+performance_rate_p = 0.9
+performance_rate_n = 0.05
+quality_dict = generate_submission(user_file, manager_file, quality_file, feedback_file, submit_file, submission_rate_f, submission_rate_m, feedback_equal_p, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, quality_rate)
+generate_performance(quality_dict, performance_rate_p, performance_rate_n, performance_file)
 
 
 # In[ ]:
