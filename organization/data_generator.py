055b2dbdca8cd37d8ea1cbf98ef6bfcc980cd3cf
Golnoosh Farnadi <golnoosh@Golnooshs-MacBook-Pro.local>
2019-07-30 07:57:12 +0200

    fix generator

--- organization/data_generator.py
+++ organization/data_generator.py
@@ -101,46 +101,48 @@ def generate_feedback(user_dict, quality_dict, manager_dict,feedback_equal_mp, f
                                         else:
                                             print('%s\t%s\t0'%(user1,user2), file =ff)
                                             feedback_dict[(user1,user2)] = 0
-                        else:     
                             
-                            if quality_dict[user2]==1:
-                                if label1==label2:
-                                    if random.random()<feedback_equal_p:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 1
+                            else:     
+                            
+                                if quality_dict[user2]==1:
+                                    if label1==label2:
+                                        if random.random()<feedback_equal_p:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
                                     else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 0
+                                        if random.random()<feedback_notequal_p:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
                                 else:
-                                    if random.random()<feedback_notequal_p:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 1
+                                    if label1==label2:
+                                        if random.random()<feedback_equal_n:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
                                     else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 0
-                            else:
-                                if label1==label2:
-                                    if random.random()<feedback_equal_n:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 1
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 0
-                                else:
-                                    if random.random()<feedback_notequal_n:
-                                        print('%s\t%s\t1'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 1
-                                    else:
-                                        print('%s\t%s\t0'%(user1,user2), file =ff)
-                                        feedback_dict[(user1,user2)] = 0
+                                        if random.random()<feedback_notequal_n:
+                                            print('%s\t%s\t1'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 1
+                                        else:
+                                            print('%s\t%s\t0'%(user1,user2), file =ff)
+                                            feedback_dict[(user1,user2)] = 0
     return feedback_dict
 
 
 # In[6]:
 
 
-def generate_submission(user_file, manager_file, quality_file, feedback_file, submit_file, submission_rate_f, submission_rate_m, feedback_equal_p, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, quality_rate):
+def generate_submission(user_file, ingroup_file, manager_file, quality_file, feedback_file, submit_file, submission_rate_A, submission_rate_B, feedback_equal_p, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, quality_rate):
     user_dict = read_user_data(user_file)
+    generate_ingroup(user_dict, ingroup_file)
     quality_dict = generate_qualification(user_dict, quality_file, quality_rate)
     manager_dict = read_manager_data(manager_file)
     feedback_dict = generate_feedback(user_dict, quality_dict, manager_dict,feedback_equal_mp, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, feedback_equal_p, feedback_equal_n, feedback_notequal_p, feedback_notequal_n, feedback_file)
@@ -148,11 +150,11 @@ def generate_submission(user_file, manager_file, quality_file, feedback_file, su
         for feedback, opinion in feedback_dict.items():
             user1 = feedback[0]
             user2 = feedback[1]
-            if user_dict[user1] =='f':
-                if random.random()<submission_rate_f:
+            if user_dict[user1] =='A':
+                if random.random()<submission_rate_A:
                     print('%s\t%s\t%d'%(user1,user2,opinion), file =sf)
             else:
-                if random.random()<submission_rate_m:
+                if random.random()<submission_rate_B:
                     print('%s\t%s\t%d'%(user1,user2,opinion), file =sf)
     return quality_dict
 
@@ -181,12 +183,26 @@ def generate_performance(quality_dict, performance_rate_p, performance_rate_n, p
 # In[8]:
 
 
+def generate_ingroup(user_dict, ingroup_file):
+    with open(ingroup_file, 'w') as igf:
+        for user1, label1 in user_dict.items():
+            for user2, label2 in user_dict.items():
+                if label1==label2:
+                    print('%s\t%s\t%d'%(user1,user2,1), file =igf)
+                else:
+                    print('%s\t%s\t%d'%(user1,user2,0), file =igf)
+
+
+# In[9]:
+
+
 user_file = './data/gender.txt'
 quality_file = './data/1/quality.txt'
 feedback_file='./data/1/feedback.txt'
 submit_file = './data/1/submit.txt'
 performance_file = './data/1/performance.txt'
-manager_file='./data/manages.txt'
+manager_file='./data/manager.txt'
+ingroup_file = './data/1/ingroup.txt'
 feedback_equal_mp = 0.9
 feedback_equal_mn = 0.05
 feedback_notequal_mp =0.6
@@ -195,12 +211,12 @@ feedback_equal_p = 0.8
 feedback_equal_n = 0.05
 feedback_notequal_p =0.5
 feedback_notequal_n =0.01
-submission_rate_f = 0.1
-submission_rate_m = 0.2
+submission_rate_A = 0.1
+submission_rate_B = 0.2
 quality_rate = 0.3
-performance_rate_p = 0.8
+performance_rate_p = 0.6
 performance_rate_n = 0.1
-quality_dict = generate_submission(user_file, manager_file, quality_file, feedback_file, submit_file, submission_rate_f, submission_rate_m, feedback_equal_p, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, quality_rate)
+quality_dict = generate_submission(user_file, ingroup_file, manager_file, quality_file, feedback_file, submit_file, submission_rate_A, submission_rate_B, feedback_equal_p, feedback_equal_mn, feedback_notequal_mp, feedback_notequal_mn, quality_rate)
 generate_performance(quality_dict, performance_rate_p, performance_rate_n, performance_file)
 
 
