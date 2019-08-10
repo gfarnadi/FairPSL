5d538198c2ca571ecc7afe6271233ac0839cad94
gfarnadi <golnoosh@Golnooshs-MBP.lan>
2019-08-10 16:50:44 -0400

    fix grounding

--- organization/fair_grounding.py
+++ organization/fair_grounding.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[29]:
 
 
 def fairGrounding(dataPath, data_folder):
@@ -26,55 +26,57 @@ def fairGrounding(dataPath, data_folder):
             
 
     # Performance --> Observed
-    Performance = dict()
+    performance_rel = dict()
     with open(dataPath+'performance.txt') as f:
         for line in f:
             line = line.strip()
             if not line: continue
             [employee, truth] = line.split()
-            Performance[employee2] = float(truth)  
+            performance_rel[employee] = (True, float(truth))
             
     # Manager --> Observed
-    manager = dict()
+    manager_to_employee = dict()
+    employee_to_manager = dict()
     with open(dataPath+'manager.txt') as f:
         for line in f:
             line = line.strip()
             if not line: continue
-            [supervisor, employee, truth] = line.split()
-            submitted_feedback[(supervisor, employee)]= (True, float(truth))
-    
-       
+            [supervisor, employee] = line.split()
+            manager_to_employee.setdefault(supervisor,[]).append(employee)
+            employee_to_manager.setdefault(employee,[]).append(supervisor)
+           
     # Ingroup --> Observed
     ingroup = dict()
     with open(dataPath+'ingroup.txt') as f:
         for line in f:
             line = line.strip()
             if not line: continue
-            ingroup[(employee1, employee2)] = (True, float(truth))
+            [employee1, employee2, truth] = line.split()
+            ingroup[(employee1, employee2)] = float(truth)
             
             
-    # Submitted Feedback --> Observed
-    submitted_feedback = dict()
+    # Submitted opinion --> Observed
+    submit_rel = dict()
     with open(dataPath+'submit.txt') as f:
         for line in f:
             line = line.strip()
             if not line: continue
             [provider, reciever, truth] = line.split()
-            submitted_feedback[(provider, reciever)]= (True, float(truth))
+            submit_rel[(provider, reciever)]= (True, float(truth))
     
-     var_id = 0
+    var_id = 0
   
-    # Feedback --> hidden
-    feedback = dict()
-    feedback_truth = dict()
-    feedback_rel = dict()
-    with open(dataPath+'feedback.txt') as f:
+    # opinion --> hidden
+    opinion = dict()
+    opinion_truth = dict()
+    opinion_rel = dict()
+    with open(dataPath+'opinion.txt') as f:
         for line in f:
             line = line.strip()
             if not line: continue
             [provider, reciever, truth] = line.split()
-            feedback_truth[(provider, reciever)] = (var_id, truth)
-            feedback_rel[(provider, reciever)] = (false, var_id)
+            opinion_truth[(provider, reciever)] = (var_id, truth)
+            opinion_rel[(provider, reciever)] = (False, var_id)
             var_id += 1
 
     # True Quality --> hidden
@@ -86,7 +88,7 @@ def fairGrounding(dataPath, data_folder):
             if not line: continue
             [employee, truth] = line.split()
             true_quality_truth[(employee)] = (var_id, truth)
-            true_quality_rel[(employee)] = (false, var_id)
+            true_quality_rel[(employee)] = (False, var_id)
             var_id += 1
               
     # Promotion -> target (inference task)
@@ -111,129 +113,120 @@ def fairGrounding(dataPath, data_folder):
     
     # 1: True_Quality(e) -> Performance(e)
     for e in employees:
-        body = [true_quality_rel[e] + (False,),
-        head = [performance[(e)] + (False,)]
+        body = [true_quality_rel[e] + (False,)]
+        head = [performance_rel[(e)] + (False,)]
         rules.append((1, body, head))
 
     # 1: ~True_Quality(e) -> ~Performance(e)
     for e in employees:
-        body = [true_quality_rel[e] + (True,),
-        head = [performance[(e)] + (True,)]
+        body = [true_quality_rel[e] + (True,)]
+        head = [performance_rel[(e)] + (True,)]
         rules.append((1, body, head))
                     
-    # \infty: Submit(m,e) -> Feedback(m,e)                  
-    for key,value in submitted_feedback.items():
-        (m,e) = key
-        body = [submitted_feedback[(m,e)]+(False,)]
-        head = [feedback_rel[(m,e)] + (False,)]
-        hardrules.append((None, body, head))
+    # \infty: Submit(e1,e2) -> Opinion(e1,e2)                  
+    for e1 in employees:
+        for e2 in employees:
+            if e1==e2: continue
+            body = [submit_rel[(e1,e2)]+(False,)]
+            head = [opinion_rel[(e1,e2)] + (False,)]
+            hardrules.append((None, body, head))
+
                 
-                
-    # \infty : ~Submit(m,e) -> ~Feedback(m,e)                  
-    for key,value in submitted_feedback.items():
-        (m,e) = key
-        body = [submitted_feedback[(m,e)]+ (True,)]
-        head = [Feedback_rel[(m,e)] + (True,)]
-        hardrules.append((None, body, head))
+    # \infty : ~Submit(e1,e2)-> ~Opinion(e1,e2)                  
+    for e1 in employees:
+        for e2 in employees:
+            if e1==e2: continue
+            body = [submit_rel[(e1,e2)]+(True,)]
+            head = [opinion_rel[(e1,e2)] + (True,)]
+            hardrules.append((None, body, head))
            
-    # 1: Quality(e)  & Employee(m) & Employee(e) -> Feedback(m,e) 
-    for e in employees:
-        for m in employees:
-            if (m==e): continue
-            body = [true_quality_rel[e]]+ (True,)] 
-            head = [feedback_rel[(m,e)]+ (True,)]
+    # 1: Quality(e1)  & Employee(e1) & Employee(e2) -> Opinion(e1,e2) 
+    for e1 in employees:
+        for e2 in employees:
+            if (e1==e2): continue
+            body = [true_quality_rel[e1]+ (True,)] 
+            head = [opinion_rel[(e1,e2)]+ (True,)]
             rules.append((1, body, head))
             
-    # 1: ~Quality(e)  & Employee(m) & Employee(e) -> ~Feedback(m,e) 
-    for e in employees:
-        for m in employees:
-            if (m==e): continue
-            body = [true_quality_rel[e]]+ (False,)] 
-            head = [feedback_rel[(m,e)]+ (False,)]
+    # 1: ~Quality(e)  & Employee(m) & Employee(e) -> ~opinion(m,e) 
+    for e1 in employees:
+        for e2 in employees:
+            if (e1==e2): continue
+            body = [true_quality_rel[e1]+ (False,)] 
+            head = [opinion_rel[(e1,e2)]+ (False,)]
             rules.append((1, body, head))                
 
     
-    #1: Feedback(u,x) & Manager(v,u) -> Feedback(v,x)
-    for key in manager.keys():
-        (v,u) = key
-        for x in employees:
-            if ((x==u) or (x==v)):continue
-            body = [feedback_rel[(v,u)]]+ (False,)] 
-            head = [feedback_rel[(v,x)]+ (False,)]
-            rules.append((1, body, head))
+    #1: Opinion(e1,e2) & Manager(m,e1) -> Opinion(m,e2)
+    for m, emp in manager_to_employee.items():
+        for e1 in emp:
+            for e2 in employees:
+                if e1==e2: continue
+                body = [opinion_rel[e1,e2]+ (False,)] 
+                head = [opinion_rel[(m,e2)]+ (False,)]
+                rules.append((1, body, head))
                 
-    #1: ~Feedback(u,x) & Manager(v,u) -> ~Feedback(v,x)  
-    for key in manager.keys():
-        (v,u) = key
-        for x in employees:
-            if ((x==u) or (x==v)):continue
-            body = [feedback_rel[(v,u)]]+ (True,)] 
-            head = [feedback_rel[(v,x)]+ (True,)]
+    #1: ~Opinion(e1,e2) & Manager(m,e1) -> ~Opinion(m,e2)
+    for m, emp in manager_to_employee.items():
+        for e1 in emp:
+            for e2 in employees:
+                if e1==e2: continue
+                body = [opinion_rel[e1,e2]+ (True,)] 
+                head = [opinion_rel[(m,e2)]+ (True,)]
+                rules.append((1, body, head))
+                
+    #1: opinion(m,e) & Manager(m,e) -> Quality(e) 
+    for m, emp in manager_to_employee.items():
+        for e in emp:
+            body = [opinion_rel[(m,e)]+ (False,)] 
+            head = [true_quality_rel[e]+ (False,)]
             rules.append((1, body, head))
     
-    #1: Feedback(m,e) & Manager(m,e) -> Quality(e) 
-    for key in manager.keys():
-        (m,e) = key
-        body = [feedback_rel[(m,e)]]+ (False,)] 
-        head = [true_quality_rel[e]+ (False,)]
-        rules.append((1, body, head))
-    
-    #1: ~Feedback(m,e) & Manager(m,e) -> ~Quality(e) 
-    for key in manager.keys():
-        (m,e) = key
-        body = [feedback_rel[(m,e)]]+ (True,)] 
-        head = [true_quality_rel[e]+ (True,)]
-        rules.append((1, body, head))    
+    #1: ~opinion(m,e) & Manager(m,e) -> ~Quality(e) 
+    for m, emp in manager_to_employee.items():
+        for e in emp:
+            body = [opinion_rel[(m,e)]+ (True,)] 
+            head = [true_quality_rel[e]+ (True,)]
+            rules.append((1, body, head))   
+                
                 
     #\infty: Quality(e) -> Promotion(e)
     for e in employees:
-        body = [true_quality_rel(e)]+ (False,)]
-        head = [promotion_rel(e)] + (False,)]
-        hardrules.append((None, body, head))
-        
-
-    #\infty: Quality(e) -> Promotion(e)
-    for e in employees:
-        body = [true_quality_rel(e)]+ (False,)]
-        head = [promotion_rel(e)] + (False,)]
+        body = [true_quality_rel(e)+ (False,)]
+        head = [promotion_rel(e) + (False,)]
         hardrules.append((None, body, head))
             
     #\infty: ~Quality(e) -> ~Promotion(e)
     for e in employees:
-        body = [true_quality_rel(e)]+ (True,)]
-        head = [promotion_rel(e)] + (True,)]
+        body = [true_quality_rel(e)+ (True,)]
+        head = [promotion_rel(e) + (True,)]
         hardrules.append((None, body, head))
                 
-    # 1: 	!Promotion(P)                
+    # 1: !Promotion(P)                
     for e in employees:
         body = []
         head = [promotion_rel[e] + (True,)]
         rules.append((1, body, head))
- 
-    # F1: ingroup(u,v) F2:manager(u,v)
+    
+    
+    #atoms = dict(review=positive_review_rel,acceptable=acceptable_rel, presents=presents_rel)
+    # F1: ingroup(e,m) F2:manager(m,e)
     # d: promotion(e)
     counts = []
-    for key in manager.keys():
-        (u,v) = key
-        F1 = ingroup[(u,v)]
-        F2 = manager[(u,v)]
-        d = promotion_rel[v]
-        counts.append((F1, F2, d))  
+    for e in employees:
+        managers_e = employee_to_manager[e]
+        for m in managers_e:
+            F1 = ingroup[(m,e)]
+            F2 = manager[(m,e)]
+            d = promotion_rel[e]
+            counts.append((F1, F2, d))  
     
-    #atoms = {}
-    #atoms['review']   =  true_quality_truth
-    #atoms['promotion']   =  promotion_truth
+    atoms = {}
+    atoms['quality']   =  true_quality_truth
+    atoms['promotion']   =  promotion_truth
     #atoms = dict(review=positive_review_rel,acceptable=acceptable_rel, presents=presents_rel)
     
     return rules, hard_rules, counts, atoms
-
-
-
-
-
-
-
-
 
 
 # In[ ]:
