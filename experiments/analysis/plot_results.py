20638f4e648bb1842ffa4cc07678cccb58ef7697
gfarnadi <golnoosh@Golnooshs-MBP.lan>
2019-08-15 20:40:15 -0400

    add details to the plot maker

--- experiments/analysis/plot_results.py
+++ experiments/analysis/plot_results.py
@@ -66,8 +66,9 @@ def plot_results(results, pdf_filename):
         _ = plt.xlabel(r'$\delta$', fontsize=15)
         _ = plt.ylabel(measure, fontsize=15)
 
-    _ = plt.figlegend(lines, labels, loc ='upper center', ncol=6, columnspacing=7, 
+    _ = plt.figlegend(lines, labels, loc ='upper center', ncol=len(results), columnspacing=7, 
                       borderaxespad=0)
+    plt.subplots_adjust(top=0.8)
     plt.savefig(pdf_filename, bbox_inches='tight')
 
 
